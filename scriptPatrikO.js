e34860bd68e3e6abdc8431b28dff812b2ed00d04
Patrik Olofsson <Patrik.d.olofsson@gmail.com>
2016-03-22 19:54:39 +0100

    Login, POST - responds ID > REDIRECT index.html

---
+++ scriptPatrikO.js
@@ -0,0 +1,29 @@
+function postForm() {
+	
+    var cust = {
+    email: $("#email").val(),
+    password: $("#password").val()
+   
+    };
+   	
+    $.ajax( {
+    type: 'POST',
+    url: 'http://nackademiska.azurewebsites.net/2/login',
+    data: cust,
+    success: function(data) {
+        alert('User ' + data.Id + " is now logged in");
+        location.href = "index.html";
+    },
+    error: function(data) {
+    	alert(JSON.stringify(data));
+        //alert('error saving delt');
+    }
+    
+    });
+}
+	
+
+$("#add").click(function(e) {
+    postForm();
+	 e.preventDefault();
+});
