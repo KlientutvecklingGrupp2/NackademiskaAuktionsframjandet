051099cf3c04c5419b5c5a23867e9d930a1c431f
JelloElks <kole.lleshaj@gmail.com>
2016-03-24 16:51:43 +0100

    Tog bort script

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ $(document).ready(function () {
         $.getJSON("http://nackademiska.azurewebsites.net/2/getongoingauctions", function (data) {
             $.each(data, function (i, auction) {
                 $("tbody").append(
-                    "<tr><td class='name'>" + auction.Name + "</td>" + "<td>" + auction.Description + "</td>" +
+                    "<tr><td>" + auction.Name + "</td>" + "<td>" + auction.Description + "</td>" +
                     "<td>" + auction.EndTime + "</td>" + "<td>" + auction.CategoryId + "</td>" +
                     "<td>" + auction.AcceptPrice +
                     "</td></tr>")
                     
-                    var options = {
-  					valueNames: [ 'name' ]
-					};var userList = new List('users', options);
+                    //var options = {
+  					//valueNames: [ 'name' ]
+					//};var userList = new List('users', options);
+                    
+                    
+                    var $rows = $('table tr');
+                    $('#search').keyup(function() {
+                        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+                        
+                        $rows.show().filter(function() {
+                            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                            return !~text.indexOf(val);
+                        }).hide();
+                    });
                     
             });
         });
@@ -24,4 +35,5 @@ $(document).ready(function () {
         $("tbody").empty();
         $("tr").hide();
     });
+    
 });
