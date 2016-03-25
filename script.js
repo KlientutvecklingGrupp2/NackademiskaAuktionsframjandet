88a01679fdf51b7a1928634847acbe1586cac2d8
JelloElks <kole.lleshaj@gmail.com>
2016-03-25 11:27:33 +0100

    Index

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("tr").hide();
+    
+    alert("hej");
 
     $("#listcurrentauctions").click(function () {
         $("tbody").empty();
@@ -11,21 +13,21 @@ $(document).ready(function () {
                     "<td>" + auction.AcceptPrice +
                     "</td></tr>")
                     
-                    var options = {
-  					valueNames: [ 'name' ]
-					};var userList = new List('users', options);
+                    //var options = {
+//  					valueNames: [ 'name' ]
+//					};var userList = new List('users', options);
                     
+					
                     
+                    var $rows = $('table tr');
+                $('#search').keyup(function() {
+                    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
                     
-                    //var $rows = $('table tr');
-                //$('#search').keyup(function() {
-                 //   var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-                    
-                 //   $rows.show().filter(function() {
-                 //       var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-                //        return !~text.indexOf(val);
-                //    }).hide();
-               // });
+                    $rows.show().filter(function() {
+                        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                        return !~text.indexOf(val);
+                    }).hide();
+                });
                     
                     
                     
--- scriptBase.js
+++ scriptBase.js
@@ -1,86 +1,9 @@
 $(document).ready(function() {
 
-    $('testrow').click(function() {
-        var href = $(this).find("a").attr("href");
-        if(href) {
-            window.location = href;
-        }
-    });
+	$('#table2').filterTable();
+
+	console.log($('#table2'));
 
 });
 
 
-/*
-
-$(document).ready(function() {
-
-    $.getJSON("http://nackademiska.azurewebsites.net/2/getcategories", function(data) {
-        $.each(data, function(i, getcategories) {
-
-				$("#body").append("<tr><td>" + getcategories.Name + "</td></tr>")
-
-    });
-
-  });
-
-});
-
-
-
-$(function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  });
-
-
-
-
-
-$(document).ready(function() {
-
-    $.getJSON("http://nackademiska.azurewebsites.net/2/getcustomers", function(data) {
-        $.each(data, function(i, deltagare) {
-
-$(function() {
-    var availableTags = [
-
-
-				$("tbody").append(deltagare.Name)
-
-    ];
-    $( "#search" ).autocomplete({
-      source: availableTags
-    });
-    });
-    });
-
-  });
-
-});
-
-*/
